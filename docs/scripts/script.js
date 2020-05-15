8d41bdae8f1be4d048bd42a4c145c74114241f63
Sergey Malinin <malininss@yandex.ru>
2020-05-15 20:34:32 +0300

    create price page, news page, service page

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,3 +1,4 @@
+// menu
 const burgerMenu = document.querySelector('.header__burger-wrapper');
 const mobileMenu = document.querySelector('.mobile-header__menu');
 const mobileBackground = document.querySelector('.mobile-header__background');
@@ -15,7 +16,7 @@ mobileBackground.addEventListener('click', () => {
 
 
 
-////
+//gallery
 
 const prevArrow = document.querySelector('.gallery__prev-arrow');
 const nextArrow = document.querySelector('.gallery__next-arrow');
@@ -23,46 +24,84 @@ const nextArrow = document.querySelector('.gallery__next-arrow');
 const mainPicture = document.querySelector('.gallery__main-pic img');
 const smallPictures = Array.from(document.querySelectorAll('.gallery__prev-pic'));
 
+if (mainPicture && smallPictures && prevArrow && nextArrow) {
+  let activePictureIndex = 0;
 
-let activePictureIndex = 0;
+  const updateActivePicture = () => {
+    smallPictures.forEach(elem => {
+      if (elem.classList.contains('active')) {
+        elem.classList.remove('active');
+      }
+    });
 
-const updateActivePicture = () => {
-  smallPictures.forEach(elem => {
-    if (elem.classList.contains('active')) {
-      elem.classList.remove('active');
+    if (activePictureIndex < 0) {
+      activePictureIndex = smallPictures.length - 1;
     }
+
+    if (activePictureIndex > smallPictures.length - 1) {
+      activePictureIndex = 0;
+    }
+
+    mainPicture.src = smallPictures[activePictureIndex].querySelector('img').src;
+    smallPictures[activePictureIndex].classList.add('active');
+    
+  }
+
+  updateActivePicture();
+
+
+  smallPictures.forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+      activePictureIndex = smallPictures.indexOf(event.target.parentElement)
+      updateActivePicture();
+    });
   });
 
-  if (activePictureIndex < 0) {
-    activePictureIndex = smallPictures.length - 1;
-  }
 
-  if (activePictureIndex > smallPictures.length - 1) {
-    activePictureIndex = 0;
-  }
+  prevArrow.addEventListener('click', () => {
+    activePictureIndex--;
+    updateActivePicture();
+  });
 
-
-  mainPicture.src = smallPictures[activePictureIndex].querySelector('img').src;
-  smallPictures[activePictureIndex].classList.add('active');
+  nextArrow.addEventListener('click', () => {
+    activePictureIndex++;
+    updateActivePicture();
+  });
 }
 
-updateActivePicture();
 
-smallPictures.forEach((elem) => {
+// price;
 
-  elem.addEventListener('click', (event) => {
-    activePictureIndex = smallPictures.indexOf(event.target.parentElement)
-    updateActivePicture();
+const priceLinks = Array.from(document.querySelectorAll('.price__link-container'));
+const priceDescriptionContainers = Array.from(document.querySelectorAll('.price__description-container'));
+
+// let activePriceInex = 0;
+
+const updateActivePrice = (activePriceIndex = 0) => {
+  priceLinks.forEach((elem, index) => {
+    if (elem.classList.contains('price__link-container_active')) {
+      elem.classList.remove('price__link-container_active');
+    }
   })
-});
+
+  priceDescriptionContainers.forEach((elem, index) => {
+    if (elem.classList.contains('price__description-container_active')) {
+      elem.classList.remove('price__description-container_active');
+    }
+  })
+
+  priceLinks[activePriceIndex].classList.add('price__link-container_active');
+  priceDescriptionContainers[activePriceIndex].classList.add('price__description-container_active');
+
+};
 
 
-prevArrow.addEventListener('click', () => {
-  activePictureIndex--;
-  updateActivePicture();
-})
+priceLinks.forEach(elem => {
+  elem.addEventListener('click', (event) => {
+    const currentElemIndex = priceLinks.indexOf(event.target.parentElement);
+    if (currentElemIndex >= 0) {
+      updateActivePrice(currentElemIndex);
+    }
 
-nextArrow.addEventListener('click', () => {
-  activePictureIndex++;
-  updateActivePicture();
+  })
 })
