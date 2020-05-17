4cff0cd3b21e208deee0c08a8826c7a266d309f8
Sergey Malinin <malininss@yandex.ru>
2020-05-17 20:38:20 +0300

    disabling the hover in the mobile version

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,4 +1,9 @@
-document.addEventListener("touchstart", function(){}, true);
+if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+  console.log('this is a touch device');
+} else {
+  console.log('this is not a touch device');
+  document.body.classList.add('no-touch');
+}
 
 // menu
 const burgerMenu = document.querySelector('.header__burger-wrapper');
